64fc145c65887d532349d0005dfe751a47ca9a20
hossam <hossamkotp89@gmail.com>
2020-02-11 20:16:47 +0200

    create hossam Activiy

---
+++ app/src/main/java/com/mokhtar/app1/HossamActivity.kt
@@ -0,0 +1,12 @@
+package com.mokhtar.app1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HossamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hossam)
+    }
+}
